f7f7399c1e34ad3351877b7da61391fbc1b0772b
rizwanzaman65 <rizwan2203e@aptechgdn.net>
2024-12-14 12:19:53 +0500

    digi

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// JavaScript for Hamburger Menu
+document.getElementById("hamburger").addEventListener("click", function () {
+  const navLinks = document.getElementById("nav-links");
+  navLinks.classList.toggle("active");
+});
